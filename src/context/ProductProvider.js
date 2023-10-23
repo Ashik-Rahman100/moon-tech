2e32b90e264d708e21f906e1589e923bc6910d88
Ashik-Rahman100 <ashik5515@gmail.com>
2023-10-23 18:47:10 +0600

    start project

---
+++ src/context/ProductProvider.js
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const PRODUCT_CONTEXT = createContext()
+
+const ProductProvider = ({children}) => {
+  const [data , setData] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:5000/data')
+    .then(res => res.json())
+    .then(data => setData(data))
+  },[])
+
+  const value = {
+    data
+  }
+  return (
+    <div>
+      <PRODUCT_CONTEXT.Provider value={value}>
+        {children}
+      </PRODUCT_CONTEXT.Provider>
+    </div>
+  );
+};
+
+export const useProducts = () =>{
+  const context = useContext(PRODUCT_CONTEXT);
+  return context
+}
+
+export default ProductProvider;
